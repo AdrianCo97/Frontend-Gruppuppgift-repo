b19dbfde1a8cb42103e67833b62ec67b00569b80
AdrianCo97 <69801867+AdrianCo97@users.noreply.github.com>
2021-02-21 14:43:45 +0100

    Städade upp kod i bokningar2.js
    Jag bytte ifrån if-else till en switch case meny för att det ser mycket renare ut.

--- Asset/JS/bokningar2.js
+++ Asset/JS/bokningar2.js
@@ -7,58 +7,65 @@ $(document).ready(function () {
     })
   });
 
-
   $('#movie').change(function () {
     let selection = $(this).val();
     $('#ticket').empty(); //Varje gång selecten movie ändras så töms tickets selecten.
     $('#date').empty();
 
-    if (selection === 'After we collided') {
-      matureMovieticketWriter();
 
-      $.getJSON('/JSON/showings.json', function (json) {
-        for (let i = 0; i < json.showings.afterWeCollided.length; i++) {
-          $('#date').append(`<option value="${json.showings.afterWeCollided[i].date}, ${json.showings.afterWeCollided[i].time}">${json.showings.afterWeCollided[i].date}, ${json.showings.afterWeCollided[i].time}</option>`);
-        }
-      })
-    }
-    else if (selection === 'Alfons leker Einstein') {
-      childMovieticketWriter();
+    switch (selection) {
+      case 'After we collided':
+        matureMovieticketWriter();
 
-      $.getJSON('/JSON/showings.json', function (json) {
-        for (let i = 0; i < json.showings.alfonsLekerEinstein.length; i++) {
-          $('#date').append(`<option value="${json.showings.alfonsLekerEinstein[i].date}, ${json.showings.alfonsLekerEinstein[i].time}">${json.showings.alfonsLekerEinstein[i].date}, ${json.showings.alfonsLekerEinstein[i].time}</option>`);
-        };
-      });
-    }
-    else if (selection === 'Andra sidan') {
-      matureMovieticketWriter();
+        $.getJSON('/JSON/showings.json', function (json) {
+          for (let i = 0; i < json.showings.afterWeCollided.length; i++) {
+            $('#date').append(`<option value="${json.showings.afterWeCollided[i].date}, ${json.showings.afterWeCollided[i].time}">${json.showings.afterWeCollided[i].date}, ${json.showings.afterWeCollided[i].time}</option>`);
+          }
+        })
+        break;
 
-      $.getJSON('/JSON/showings.json', function (json) {
-        for (let i = 0; i < json.showings.andraSidan.length; i++) {
-          $('#date').append(`<option value="${json.showings.andraSidan[i].date},${json.showings.andraSidan[i].time}">${json.showings.andraSidan[i].date}, ${json.showings.andraSidan[i].time}</option>`);
-        };
-      });
-    }
-    else if (selection === 'Berts dagbok') {
-      childMovieticketWriter();
+      case 'Alfons leker Einstein':
+        childMovieticketWriter();
 
-      $.getJSON('/JSON/showings.json', function (json) {
-        for (let i = 0; i < json.showings.bertsDagbok.length; i++) {
-          $('#date').append(`<option value="${json.showings.bertsDagbok[i].date},${json.showings.bertsDagbok[i].time}">${json.showings.bertsDagbok[i].date}, ${json.showings.bertsDagbok[i].time}</option>`);
-        };
-      });
-    }
-    else if (selection === 'Come play') {
-      matureMovieticketWriter();
+        $.getJSON('/JSON/showings.json', function (json) {
+          for (let i = 0; i < json.showings.alfonsLekerEinstein.length; i++) {
+            $('#date').append(`<option value="${json.showings.alfonsLekerEinstein[i].date}, ${json.showings.alfonsLekerEinstein[i].time}">${json.showings.alfonsLekerEinstein[i].date}, ${json.showings.alfonsLekerEinstein[i].time}</option>`);
+          };
+        });
+        break;
 
-      $.getJSON('/JSON/showings.json', function (json) {
-        for (let i = 0; i < json.showings.comePlay.length; i++) {
-          $('#date').append(`<option value="${json.showings.comePlay[i].date},${json.showings.comePlay[i].time}">${json.showings.comePlay[i].date}, ${json.showings.comePlay[i].time}</option>`);
-        };
-      });
+      case 'Andra sidan':
+        matureMovieticketWriter();
+
+        $.getJSON('/JSON/showings.json', function (json) {
+          for (let i = 0; i < json.showings.andraSidan.length; i++) {
+            $('#date').append(`<option value="${json.showings.andraSidan[i].date},${json.showings.andraSidan[i].time}">${json.showings.andraSidan[i].date}, ${json.showings.andraSidan[i].time}</option>`);
+          };
+        });
+        break;
+
+      case 'Berts dagbok':
+        childMovieticketWriter();
+
+        $.getJSON('/JSON/showings.json', function (json) {
+          for (let i = 0; i < json.showings.bertsDagbok.length; i++) {
+            $('#date').append(`<option value="${json.showings.bertsDagbok[i].date},${json.showings.bertsDagbok[i].time}">${json.showings.bertsDagbok[i].date}, ${json.showings.bertsDagbok[i].time}</option>`);
+          };
+        });
+        break;
+
+      case 'Come play':
+        matureMovieticketWriter();
+
+        $.getJSON('/JSON/showings.json', function (json) {
+          for (let i = 0; i < json.showings.comePlay.length; i++) {
+            $('#date').append(`<option value="${json.showings.comePlay[i].date},${json.showings.comePlay[i].time}">${json.showings.comePlay[i].date}, ${json.showings.comePlay[i].time}</option>`);
+          };
+        });
+        break;
     }
-  });
+
+  })
 })
 
 function matureMovieticketWriter() {
