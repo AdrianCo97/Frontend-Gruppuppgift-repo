08bb2ef6bbc6f8b2329f6bd7aa462ce3b1fdc8e0
dennishoang2010 <61258188+dennishoang2010@users.noreply.github.com>
2021-03-05 11:18:22 +0100

    Structure change

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-
-// Create a new web server (based on express)
-
-const app = express();
-
-// Server all the files in the www folder (make new folder for all frontend material)
-app.use(express.static('Asset'));
-
-// Start the webserver on port 3000
-app.listen(3000, () => console.log('Listening on port 3000'));
+require('best-sqlite3-frontend')({
+  bestSqlite3: require('best-sqlite3'),
+  databasePath: '/asset/databases/chinook.db',
+  addDatabaseFunctions: {
+    // example of user defined functions
+    // (write your own as you go...)
+    UP: x => x.toUpperCase(),
+    LOW: x => x.toLowerCase()
+  },
+  express: require('express'),
+  port: 3000,
+  staticFolder: 'www'
+}).then(({ app, db }) => {
+  // if you want to you can
+  // do more things with the express app 
+  // and the db connection here...
+});
